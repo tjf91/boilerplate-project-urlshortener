ae02fef13c78c804555168477fcb20268b6f5f3e
tjf91 <tudorjflorea@gmail.com>
2021-02-21 08:46:21 -0700

    fixed some issues to pass freecodecamp test, could use a little more work before hosting

--- urlController.js
+++ urlController.js
@@ -1,37 +1,59 @@
 const dns = require('dns');
 const mongoose = require('mongoose');
 var adler32 = require('adler32');
+const axios = require('axios')
 
 const urlsSchema = new mongoose.Schema({
-    original: String,
-    short: String
+    original_url: String,
+    short_url: String
   });
     const Url = mongoose.model('url', urlsSchema)
 
 module.exports={
     addUrl:async(req,res)=>{
+//       const options = {  
+//   hints: dns.ADDRCONFIG | dns.V4MAPPED,
+// };
+        const saveUrl=()=>{
+            const original_url=req.body.url
+                const short_url=adler32.sum(original_url).toString(16)
+                const newUrl= new Url({original_url,short_url})
+                newUrl.save()
+	            .then(()=> res.json({original_url,short_url}))
+	            .catch(err => res.status(400).json('Error: '+ err));
+
+        }
         console.log('addUrl', req.body)
-        dns.lookup(req.body.url, (err,address,family)=>{
+       await dns.lookup(req.body.url, async (err)=>{
             if(err){
-                return res.json({error: 'invalid url'})
+
+               await axios.get(err.hostname).then(res=>{
+                 console.log('this is status code',res.status)
+                 if(res.status!==200){
+                   res.json({error: 'invalid url'})             
+
+                 }
+
+            else{
+                saveUrl()
+            }
+
+                 
+               }).catch(e=>res.json({error: 'invalid url'}) )
             }
             else{
-                console.log(err,address,family)
-                const original=req.body.url
-                const short=adler32.sum(original).toString(16)
-                const newUrl= new Url({original,short})
-                newUrl.save()
-	            .then(()=> res.json({original,short}))
-	            .catch(err => res.status(400).json('Error: '+ err));
+              saveUrl()
             }
+
+            
         })
     },
     getUrl: async(req,res)=>{
         const {short}=req.params
         console.log(short)
-        Url.find({short}).then(e=>{
+       Url.findOne({short_url:short}).exec().then(e=>{
             console.log(e)
-            res.redirect('http://'+e[0].original)}).catch(err => res.status(400).json('Error: '+ err));       
+            res.redirect(e.original_url)}).catch(err => res.status(400).json('Error: '+ err));        
         
     }
 }
